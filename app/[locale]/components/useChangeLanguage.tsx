d830c0309895353bb44f79d42b182956a12a5e28
AroCodes <arocodes@gmail.com>
2025-07-20 16:56:56 +0400

    `useChangeLangauge.tsx` Fix

--- app/[locale]/components/useChangeLanguage.tsx
+++ app/[locale]/components/useChangeLanguage.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 export const useChangeLanguage = () => {
   const pathname = usePathname();
+  const router = useRouter();
 
   const changeLanguage = (lng: string) => {
     const segments = pathname.split('/').filter(Boolean);
@@ -16,7 +17,10 @@ export const useChangeLanguage = () => {
     }
 
     const newPath = '/' + segments.join('/');
-    window.location.href = newPath;
+    document.cookie = `NEXT_LOCALE=${lng}; path=/; max-age=31536000`;
+
+    router.replace(newPath);
+    router.refresh();
   };
 
   return changeLanguage;
